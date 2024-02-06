56d93873b81e0881d2d617800ee5dffd121aab65
ins <alex.inspektor@gmail.com>
2024-02-06 23:19:43 +0000

    faq section to options

--- src/components/main/faq-section/index.tsx
+++ src/components/main/faq-section/index.tsx
@@ -2,16 +2,16 @@ import options from "@/app/options";
 import QuestionAnswer from "./question-answer";
 
 function FaqSection() {
-    const { faq } = options;
+    const { title, questions } = options.faq;
     return <div className="relative mb-[76px] pt-[50px] pb-0">
         <img className="absolute right-0 top-[40px] z-[-1]" src="/images/l8.svg" alt="decor leaf" />
         <div className="px-[15px] max-w-[1260px] mx-auto my-0">
             <h2 className="mb-[27px] font-[museo] font-bold text-[40px] leading-[46px] text-center text-[#444b54] relative
             before:top-[-23px] before:left-0 before:right-0 before:my-0 before:mx-auto before:absolute before:content-[''] before:w-[96px] before:h-[2px] before:bg-[#ccc]">
-                PREGUNTAS FRECUENTES Y SUS RESPUESTAS
+                {title}
             </h2>
             <div className="flex flex-col">
-                {faq.map((questionInfo, i) => <QuestionAnswer key={i} {...questionInfo} />)}
+                {questions.map((questionInfo, i) => <QuestionAnswer key={i} {...questionInfo} />)}
             </div>
         </div>
     </div>;
