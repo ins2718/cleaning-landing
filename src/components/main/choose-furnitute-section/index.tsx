2ffd02662932a84c84f59d5084fb4c5ef287a937
ins <alex.inspektor@gmail.com>
2024-02-11 20:14:58 +0000

    update image style in choose furniture section

--- src/components/main/choose-furnitute-section/index.tsx
+++ src/components/main/choose-furnitute-section/index.tsx
@@ -8,7 +8,7 @@ function ChooseFurnitureSection() {
     return <Section header={title} footer={<ButtonWhatsApp />}>
         <h2 className="text-center text-inherit leading-[40px] text-[32px] mb-[13px] font-bold">{subtitle}</h2>
         <div className="flex flex-wrap lg:flex-nowrap">
-            <img className="max-w-[480px] mx-auto" src="/images/choose-furniture/all-furniture.png" alt="all-furniture" />
+            <img className="max-w-[480px] mx-auto w-full" src="/images/choose-furniture/all-furniture.png" alt="all-furniture" />
             <div className="max-w-[480px] mx-auto px-[10px] font-bold text-[32px] leading-[40px] flex justify-center flex-col">
                 <RightSide />
             </div>
