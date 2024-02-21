6854287a31d582fa09d3c3c29e1d83a0a96b8602
ins <alex.inspektor@gmail.com>
2024-02-21 21:36:47 +0000

    fix compared images vertical scroll

--- src/components/main/our-works-section/compared-images.tsx
+++ src/components/main/our-works-section/compared-images.tsx
@@ -13,8 +13,13 @@ function ComparedImages({ className = "", firstImage, secondImage, firstAltText 
     const { originalSrc: firstImageSrc, srcSet: firstImageSrcSet } = calcImageProps({ src: firstImage });
     const { originalSrc: secondImageSrc, srcSet: secondImageSrcSet } = calcImageProps({ src: secondImage });
     const isTouchDevice = window.matchMedia('(pointer: coarse)').matches;
+    const onPointerMove = ({ movementY, pointerType }: React.PointerEvent<HTMLDivElement>) => {
+        if (isTouchDevice && pointerType === "touch" && Math.abs(movementY) > 2) {
+            window.scrollBy({ top: 20 * movementY, behavior: "smooth" });
+        }
+    };
     return <ReactCompareSlider className={className} onlyHandleDraggable={isTouchDevice}
-        onPointerMove={(e) => { isTouchDevice && e.pointerType === "touch" && window.scrollBy({ top: e.movementY }); }}
+        onPointerMove={onPointerMove}
         itemOne={<ReactCompareSliderImage src={firstImageSrc} srcSet={firstImageSrcSet} alt={firstAltText} />}
         itemTwo={<ReactCompareSliderImage src={secondImageSrc} srcSet={secondImageSrcSet} alt={secondAltText} />}
     />;
--- src/components/main/video-section/gallery-item.tsx
+++ src/components/main/video-section/gallery-item.tsx
@@ -6,17 +6,18 @@ import { openVideoGallery } from "@/reducers/header-reducer";
 type GalleryItemProps = {
     url: string;
     index: number;
-}
+};
 
 function GalleryItem({ url, index }: GalleryItemProps) {
     const dispatch = useAppDispatch();
+    const onClick = (event: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        event.preventDefault();
+        dispatch(openVideoGallery(index));
+    };
     return <div style={{ backgroundImage: `url(${calcImageOptimizedSrc(url)})` }} className="my-[10px] mx-auto min-[720px]:mx-[10px] w-[calc(83.4%-10px)] min-[720px]:w-[calc(50%-20px)] relative h-[363px] bg-cover bg-center cursor-pointer break-words">
         <a
             href="#"
-            onClick={(e) => {
-                e.preventDefault();
-                dispatch(openVideoGallery(index))
-            }}
+            onClick={onClick}
             className="flex absolute size-full left-0 top-0 justify-center items-center duration-300
     after:size-full after:opacity-0 after:transition-opacity after:hover:opacity-100 after:duration-300 after:left-0 after:top-0 after:absolute after:content-[''] after:bg-[linear-gradient(0deg,rgba(27,136,216,0.68),rgba(27,136,216,0.68))]"
         >
