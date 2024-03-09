a68113ece2705b4a484b20f539890cd064c8c484
ins <alex.inspektor@gmail.com>
2024-03-09 02:06:43 +0000

    add fb pixel

---
+++ src/components/header/counters.tsx
@@ -0,0 +1,12 @@
+import { YMInitializer } from "react-yandex-metrika";
+import options from "@/app/options";
+import ReactPixel from 'react-facebook-pixel';
+
+function Counters() {
+    const { facebookPixelId, yandexMetricaIds } = options;
+    ReactPixel.init(facebookPixelId);
+    ReactPixel.pageView();
+    return <YMInitializer accounts={yandexMetricaIds} version="2" />;
+}
+
+export default Counters;
