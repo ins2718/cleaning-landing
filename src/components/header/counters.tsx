d3ab1119d72a9619771939c1ff28756bbb71d64e
ins <alex.inspektor@gmail.com>
2024-03-13 20:07:40 +0000

    add yandex metrica options

--- src/components/header/counters.tsx
+++ src/components/header/counters.tsx
@@ -6,7 +6,7 @@ function Counters() {
     const { facebookPixelId, yandexMetricaIds } = options;
     ReactPixel.init(facebookPixelId);
     ReactPixel.pageView();
-    return <YMInitializer accounts={yandexMetricaIds} version="2" />;
+    return <YMInitializer accounts={yandexMetricaIds} options={{ webvisor: true, defer: true }} version="2" />;
 }
 
 export default Counters;
