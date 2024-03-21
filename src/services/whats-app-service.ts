2ca42736e46fc2bf95f5aa3f73ccb374dfd505ea
ins <alex.inspektor@gmail.com>
2024-03-21 09:32:38 +0000

    fix whatsApp link

--- src/services/whats-app-service.ts
+++ src/services/whats-app-service.ts
@@ -6,7 +6,7 @@ export default class WhatsAppService {
         const { phone, whatsAppPlaceholder } = options;
         const url = `https://wa.me/${new PhoneService(phone).getPhoneNumberWithoutPlus()}?text=${encodeURIComponent(whatsAppPlaceholder)}`;
         const params = new URLSearchParams(typeof window === "undefined" ? "localhost" : window.location.search);
-        const fbclid = params.get("fbclid");
+        const fbclid = params.get("fbclid") ?? '';
         const utm: string[] = [];
         params.forEach((value, key) => key.substring(0, 3) === "utm" && utm.push(`${key}: ${value}`));
         return `//pro-chistka-mebeli.ru/landing-redirect.php?url=${encodeURIComponent(url)}&fbclid=${fbclid}&utm=${encodeURIComponent(utm.join(', '))}`;
