95c09f48416cff2721c3824b53b94ddd8b6589c5
ins <alex.inspektor@gmail.com>
2024-05-29 12:48:14 +0300

    change landing form address

--- src/app/api/order-form.ts
+++ src/app/api/order-form.ts
@@ -4,7 +4,7 @@ export const orderForm = api.injectEndpoints({
   endpoints: (builder) => ({
     sendOrderForm: builder.mutation<SendOrderFormResponse, SendOrderForm>({
       query: (chatInfo) => ({
-        url: '//pro-chistka-mebeli.ru/landing-form.php',
+        url: '//dima.limpiarmuebles.pro/landing-form.php',
         method: 'POST',
         body: chatInfo,
       }),
