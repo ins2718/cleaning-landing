5344e0a4e888429e1ec6f4b01c374b462303c2d8
Dmitriy <nadim200763@gmail.com>
2024-09-06 13:46:57 +0300

    update 1 text

--- src/components/main/first-section/left-side.tsx
+++ src/components/main/first-section/left-side.tsx
@@ -9,9 +9,9 @@ function LeftSide() {
             {title}
         </h1>
         <p className="text-[16px] md:text-[24px] leading-[24px] md:leading-[47px] font-semibold text-left font-secondary">
-            <strong className="">¿Tus muebles, colchón o alfombra están sucios, huelen mal o simplemente necesitan una limpieza a fondo?</strong>
+            <strong className="">¿Necesitas un lavado de muebles, colchón o alfombra profundo?</strong>
             <br />
-            ¡No los tires!<br />En limpiarmuebles.pro ofrecemos un servicio profesional de limpieza de muebles a domicilio que les dará una nueva vida.
+            ¡No los tires!<br />En limpiarmuebles.pro, ofrecemos servicios de lavado de muebles a domicilio con resultados profesionales.<br /> Utilizamos técnicas avanzadas, incluyendo limpieza con vapor si es necesario, para renovar tus muebles, colchones y alfombras. <br />Eliminamos manchas, olores, bacterias, ácaros de polvo y alergénicos, devolviéndoles su frescura original.
         </p>
         <div className="flex mt-[60px] items-center justify-center">
             {/* <Button content="калькулятор стоимости" className="border-[#e23936] bg-[#e23936] hover:shadow-[0_10px_15px_rgba(226,57,54,0.22)]" /> */}
