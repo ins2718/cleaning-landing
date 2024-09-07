70669af9ecd8ceedc46c8f292f93a69e25889af8
Dmitriy <nadim200763@gmail.com>
2024-09-07 12:45:44 +0300

    text2

--- src/app/options.ts
+++ src/app/options.ts
@@ -25,7 +25,7 @@ const options = {
     ],
 
     firstSection: {
-        title: "Limpieza de Sofás de Piel a Domicilio en Madrid. 10 Años de Experiencia con Productos Natuzzi",
+        title: "Limpieza de Sofás de Piel a Domicilio en Madrid.",
     },
 
     chooseFurnitureSection: {
