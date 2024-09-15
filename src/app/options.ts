6b0ed6e7c6d96a3e627a21661edc7c327acd0a67
Alex <alex.inspektor@gmail.com>
2024-09-15 19:31:20 +0300

    update first section

--- src/app/options.ts
+++ src/app/options.ts
@@ -29,6 +29,13 @@ const options = {
         showTitlePicture: false,
         showLogoTitle: false,
         showGallery: false,
+        topMargin: "130px",
+        bottomMargin: "170px",
+        leftSideText: `[b]¿Sabías que limpiar tu colchón por ti mismo puede ser ineficaz y hasta dañino?[/b]
+En limpiarmuebles.pro, entendemos que la limpieza de colchones requieIntentar limpiar un colchón con vaporeta o métodos caseros puede no solo ser ineficaz, sino también empeorar la situación al dañar el material o no eliminar ácaros, bacterias y alérgenos de manera adecuada.re un enfoque profesional.
+
+Ofrecemos servicios profesionales de lavado de colchones a domicilio, utilizando técnicas y equipos especializados que garantizan una limpieza profunda y segura.Recupera la frescura de tu colchón y disfruta de un entorno más limpio y saludable para descansar.`,
+        leftSideButtonMarginTop: "60px",
     },
 
     chooseFurnitureSection: {
--- src/components/main/first-section/left-side.tsx
+++ src/components/main/first-section/left-side.tsx
@@ -1,27 +1,30 @@
 import options from "@/app/options";
 import ButtonOrder from "@/components/buttons/button-order";
 import Image from "@/components/Image";
-import img from "/public/images/first/title-pickture.png";
+import img from "/public/images/first/title-picture.png";
+import BBCode from '@bbob/react';
+import presetReact from '@bbob/preset-react';
+import { CSSProperties } from "react";
 import CompareImagesSlider from "../our-works-section/compare-images-slider";
 
+const plugins = [presetReact()];
+
 function LeftSide() {
-    const { title, showTitlePicture, showLogoTitle, showGallery } = options.firstSection;
-    const styles = showTitlePicture || showGallery ? { width: "100%", paddingRight: 0 } : {};
-    const h1Styles = showTitlePicture || showGallery ? { width: "100%" } : {};
-    return <div style={styles} className="pr-0 min-[1170px]:pr-[117px] mx-auto my-0 text-center lg:text-left w-[830px] relative z-[4]">
-        <h1 style={h1Styles} className="text-[30px] leading-[40px] w-auto sm:text-[40px] sm:leading-[50px] md:text-[50px] md:leading-[60px] lg:w-[716px] text-[#444b54] font-primary relative flex flex-col text-left min-[480px]:text-center">
+    const { title, showTitlePicture, showLogoTitle, leftSideText, leftSideButtonMarginTop, showGallery } = options.firstSection;
+    const styles = {
+        "--LeftSide-leftSideButtonMarginTop": leftSideButtonMarginTop,
+    };
+    return <div style={styles as CSSProperties} className="pr-0 min-[1170px]:pr-[117px] mx-auto my-0 text-center lg:text-left w-[830px] relative z-[4]">
+        <h1 className="text-[30px] leading-[40px] w-auto sm:text-[40px] sm:leading-[50px] md:text-[50px] md:leading-[60px] lg:w-[716px] text-[#444b54] font-primary relative flex flex-col text-left min-[480px]:text-center">
             {showLogoTitle && <span className="relative w-max before:content-[''] before:absolute before:left-0 before:bottom-[5px] md:before:bottom-[14px] before:z-[-1] before:h-[9px] md:before:h-[22px] before:w-full before:bg-[#d1e8fa] mx-0 min-[480px]:mx-auto">limpiarmuebles.pro</span>}
             {title}
         </h1>
         {showTitlePicture && <Image img={img} alt=""></Image>}
         {showGallery && <CompareImagesSlider />}
         <p className="text-[16px] md:text-[24px] leading-[24px] md:leading-[47px] font-semibold text-left font-secondary">
-            <strong className="">¿Sabías que limpiar tu colchón por ti mismo puede ser ineficaz y hasta dañino?</strong>
-            <br />
-            En limpiarmuebles.pro, entendemos que la limpieza de colchones requieIntentar limpiar un colchón con vaporeta o métodos caseros puede no solo ser ineficaz, sino también empeorar la situación al dañar el material o no eliminar ácaros, bacterias y alérgenos de manera adecuada.re un enfoque profesional.<br /><br />Ofrecemos servicios profesionales de lavado de colchones a domicilio, utilizando técnicas y equipos especializados que garantizan una limpieza profunda y segura.Recupera la frescura de tu colchón y disfruta de un entorno más limpio y saludable para descansar.
-            
-            </p>
-        <div className="flex mt-[60px] items-center justify-center">
+            <BBCode plugins={plugins}>{leftSideText}</BBCode>
+        </p>
+        <div className="flex mt-[--LeftSide-leftSideButtonMarginTop] items-center justify-center">
             {/* <Button content="калькулятор стоимости" className="border-[#e23936] bg-[#e23936] hover:shadow-[0_10px_15px_rgba(226,57,54,0.22)]" /> */}
             <ButtonOrder />
         </div>
