cf58a773e3585953fd140aba7592a1aac33fbdd2
ins <alex.inspektor@gmail.com>
2024-09-17 20:12:07 +0300

    add NEXT_PUBLIC_HOST_API variable

--- src/app/api/order-form.ts
+++ src/app/api/order-form.ts
@@ -4,7 +4,7 @@ export const orderForm = api.injectEndpoints({
   endpoints: (builder) => ({
     sendOrderForm: builder.mutation<SendOrderFormResponse, SendOrderForm>({
       query: (chatInfo) => ({
-        url: '//dima.limpiarmuebles.pro/landing-form.php',
+        url: process.env.NEXT_PUBLIC_HOST_API + '/landing-form.php',
         method: 'POST',
         body: chatInfo,
       }),
