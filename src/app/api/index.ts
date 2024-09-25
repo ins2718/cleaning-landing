00bf9b5ebc49347d05b8806555cdceb68661cbf5
ins <alex.inspektor@gmail.com>
2024-09-25 14:28:05 +0300

    add csrf

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -2,8 +2,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const emptySplitApi = createApi({
     baseQuery: fetchBaseQuery({
+        baseUrl: process.env.NEXT_PUBLIC_HOST_API,
+        credentials: "include",
         headers: {
             accept: "application/json",
+        },
+        prepareHeaders: (headers) => {
+            const token = document.cookie.match(/XSRF-TOKEN=([^\s;]+)/);
+            if (token) {
+                headers.set('X-XSRF-TOKEN', decodeURIComponent(token[1]));
+            }
+            return headers;
         },
     }),
     endpoints: () => ({}),
