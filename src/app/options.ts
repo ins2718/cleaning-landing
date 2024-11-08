aa635ea99151e886e137c98643b839a0cd72e1fd
ins <alex.inspektor@gmail.com>
2024-11-08 12:05:35 +0300

    fix image in first section & add gallery

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,8 +26,9 @@ const options = {
 
     firstSection: {
         title: "Limpieza de Sofás, Colchones y Sillones en Madrid: Servicio Profesional para Eliminar Polvo y Manchas",
-        showTitlePickture: false,
+        showTitlePicture: false,
         showLogoTitle: false,
+        showGallery: false,
     },
 
     chooseFurnitureSection: {
