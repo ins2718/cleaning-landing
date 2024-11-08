ba4de59383b1c79c6ca4a462c675d4e8116dd139
ins <alex.inspektor@gmail.com>
2024-11-08 12:05:35 +0300

    fix image in first section & add gallery

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,8 +26,9 @@ const options = {
 
     firstSection: {
         title: "Limpieza Profesional de Sillones de Chenille en Madrid: Servicios Especializados y Efectivos",
-        showTitlePickture: false,
+        showTitlePicture: false,
         showLogoTitle: false,
+        showGallery: false,
     },
 
     chooseFurnitureSection: {
