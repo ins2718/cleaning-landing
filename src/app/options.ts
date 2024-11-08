20fd41257920390a0900a2a68ad4f7c5d4ff757f
ins <alex.inspektor@gmail.com>
2024-11-08 12:05:35 +0300

    fix image in first section & add gallery

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,8 +26,9 @@ const options = {
 
     firstSection: {
         title: "Limpieza de Colchones en Madrid: Servicio para Colchones, Sofás y Sillones - Desinfección Profesional",
-        showTitlePickture: false,
+        showTitlePicture: false,
         showLogoTitle: false,
+        showGallery: false,
     },
 
     chooseFurnitureSection: {
