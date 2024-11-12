9eff66ee7fbee6e3cadd54ffd7d4667a41c4bf05
ins <alex.inspektor@gmail.com>
2024-11-12 18:32:58 +0300

    Add mute param

--- src/app/options.ts
+++ src/app/options.ts
@@ -158,6 +158,7 @@ Utilizamos productos y equipos de limpieza de alta calidad para que tus alfombra
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
         ],
+        mute: true,
     },
 
     faqSection: {
