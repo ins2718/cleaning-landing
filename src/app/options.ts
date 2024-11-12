1c80bfd4676ea1c540ad7db2cd3fbb54964d3d12
ins <alex.inspektor@gmail.com>
2024-11-12 18:32:58 +0300

    Add mute param

--- src/app/options.ts
+++ src/app/options.ts
@@ -158,6 +158,7 @@ Utilizamos productos y equipos de limpieza de alta calidad, diseñados específi
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
         ],
+        mute: true,
     },
 
     faqSection: {
