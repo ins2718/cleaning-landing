a1ae286a77499f3e6995ae39b2ca3cffddb7398c
ins <alex.inspektor@gmail.com>
2024-11-12 17:33:39 +0300

    Hide right side with gallery

--- src/components/main/first-section/index.tsx
+++ src/components/main/first-section/index.tsx
@@ -2,11 +2,16 @@ import LeftSide from "./left-side";
 import RightSide from "./right-side";
 
 function FirstSection() {
-    return <div id="first-section" className="relative mt-[130px] mb-[170px] min-h-[327px] sm:min-h-[357px] md:min-h-[479px] min-[1170px]:min-h-[570px]">
+    const { topMargin, bottomMargin, showTitlePicture, showGallery } = options.firstSection;
+    const styles = {
+        "--FirstSection-topMargin": topMargin,
+        "--FirstSection-bottomMargin": bottomMargin,
+    };
+    return <div style={styles as CSSProperties} id="first-section" className="relative mt-[--FirstSection-topMargin] mb-[--FirstSection-bottomMargin] min-h-[327px] sm:min-h-[357px] md:min-h-[479px] min-[1170px]:min-h-[570px] whitespace-pre-wrap">
         <div className="px-[15px] mx-auto my-0 max-w-[1260px]">
             <div className="flex">
                 <LeftSide />
-                <RightSide />
+                {(!showTitlePicture && !showGallery) && <RightSide />}
             </div>
         </div>
     </div>;
