9198661dceab31d5b2cb677a55a12e947566ffa0
Dmitriy <nadim200763@gmail.com>
2024-11-14 22:41:47 +0300

    video

--- src/app/options.ts
+++ src/app/options.ts
@@ -149,6 +149,9 @@ Ofrecemos servicios profesionales de lavado de colchones a domicilio, utilizando
         title: "Un vídeo que dice más que mil palabras...",
         videos: [
             {
+                original: "dy8zLn1uF20",
+                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
+            },{
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
