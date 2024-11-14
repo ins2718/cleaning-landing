05f94c1cfbcfe397e5403c74627150e9aa4a130d
Dmitriy <nadim200763@gmail.com>
2024-11-14 21:51:37 +0300

    video

--- src/app/options.ts
+++ src/app/options.ts
@@ -148,6 +148,9 @@ Recupera la frescura de tu colchón y disfruta de un entorno más limpio y salud
         title: "Un vídeo que dice más que mil palabras...",
         videos: [
             {
+                original: "dy8zLn1uF20",
+                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
+            },{
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
