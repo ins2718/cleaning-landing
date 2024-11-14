d0bb631adec39e17cffb9fefb5e041094a41243e
Dmitriy <nadim200763@gmail.com>
2024-11-14 13:25:51 +0300

    change video

--- src/app/options.ts
+++ src/app/options.ts
@@ -159,6 +159,9 @@ Utilizamos técnicas avanzadas, incluyendo limpieza de mobiliario con vapor si e
         subtitle: "",
         videos: [
             {
+                original: "dy8zLn1uF20",
+                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
+            },{
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
@@ -169,7 +172,7 @@ Utilizamos técnicas avanzadas, incluyendo limpieza de mobiliario con vapor si e
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
         ],
-        mute: true,
+        mute: false,
     },
 
     faqSection: {
