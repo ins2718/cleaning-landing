62edf3efd894d33d64b591f4d14de83c65426a82
Dmitriy <nadim200763@gmail.com>
2024-11-14 23:40:54 +0300

    v

--- src/app/options.ts
+++ src/app/options.ts
@@ -157,11 +157,9 @@ Eliminamos manchas, olores, bacterias, ácaros del polvo y alérgenos, devolvié
 
     videoSection: {
         title: "Un vídeo que dice más que mil palabras...",
+        subtitle: "",
         videos: [
             {
-                original: "dy8zLn1uF20",
-                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
-            }, {
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
@@ -171,9 +169,8 @@ Eliminamos manchas, olores, bacterias, ácaros del polvo y alérgenos, devolvié
                 original: "a2np9IDDTHQ",
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
-
         ],
-        mute: false,
+        mute: true,
     },
 
     faqSection: {
