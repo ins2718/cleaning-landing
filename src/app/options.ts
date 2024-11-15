4d4aa5159524cfc6334dacf6c4c994e222d3582f
Dmitriy <nadim200763@gmail.com>
2024-11-16 00:36:37 +0300

    mute off

--- src/app/options.ts
+++ src/app/options.ts
@@ -168,7 +168,7 @@ Utilizamos productos y equipos de limpieza profesionales para desinfectar colcho
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
         ],
-        mute: true,
+        mute: false,
     },
 
     faqSection: {
