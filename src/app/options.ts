e12ac6d4a6f8a61025cc0d599ca4ea36be21f464
Dmitriy <nadim200763@gmail.com>
2024-11-16 00:43:36 +0300

    title pic gallary

--- src/app/options.ts
+++ src/app/options.ts
@@ -125,6 +125,10 @@ const options = {
             root: '/images/our-works/compare/',
             images: [
                 {
+                    path: 'sofa_corner/',
+                    before: 'before.jpg',
+                    after: 'after.jpg',
+                }, {
                     path: 'mattress/',
                     before: 'before.jpg',
                     after: 'after.jpg',
@@ -132,11 +136,7 @@ const options = {
                     path: 'sofa_brown/',
                     before: 'before.jpg',
                     after: 'after.jpg',
-                }, {
-                    path: 'sofa_corner/',
-                    before: 'before.jpg',
-                    after: 'after.jpg',
-                }, {
+                },  {
                     path: 'sofa_corner_black/',
                     before: 'before.jpg',
                     after: 'after.jpg',
