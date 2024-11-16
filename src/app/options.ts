d62ef2d68fedfd2082b93286cb3b3060d5d1b373
Dmitriy <nadim200763@gmail.com>
2024-11-16 12:03:09 +0300

    title pic

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,7 +26,7 @@ const options = {
 
     firstSection: {
         title: "Limpieza Integral de Tapizados en Madrid - Sofás, Sillas y Colchones",
-        showTitlePicture: false,
+        showTitlePicture: true,
         showLogoTitle: false,
         showGallery: true,
         topMargin: "130px",
