cadf605629933c9ac3f85d285ee5601b422adef1
Dmitriy <nadim200763@gmail.com>
2024-11-17 13:38:36 +0300

    video

--- src/app/options.ts
+++ src/app/options.ts
@@ -145,8 +145,12 @@ const options = {
 
     videoSection: {
         title: "Un vídeo que dice más que mil palabras...",
+        subtitle: "",
         videos: [
             {
+                original: "dy8zLn1uF20",
+                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
+            },{
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
@@ -156,8 +160,9 @@ const options = {
                 original: "a2np9IDDTHQ",
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
+
         ],
-        mute: true,
+        mute: false,
     },
 
     faqSection: {
