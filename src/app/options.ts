cd4f304c98f7f7a55e4e5816a8dce4ad337b03b3
Dmitriy <nadim200763@gmail.com>
2024-11-17 13:34:20 +0300

    add title and gallary pic tab

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,14 +26,15 @@ const options = {
 
     firstSection: {
         title: "Limpieza de Colchones en Madrid: Servicio para Colchones, Sofás y Sillones - Eliminación de Manchas",
-        showTitlePicture: false,
+        showTitlePicture: true,
         showLogoTitle: false,
-        showGallery: false,
+        showGallery: true,
         topMargin: "130px",
         bottomMargin: "170px",
-        leftSideText: `[b]¿Tu colchón necesita una limpieza profunda y eliminación de manchas?[/b]
-Con más de 10 años de experiencia, en limpiarmuebles.pro ofrecemos servicios profesionales de limpieza de colchones a domicilio, especializados en la eliminación de manchas, ácaros, bacterias y alérgenos.
-Utilizamos productos y equipos de limpieza profesionales para obtener los mejores resultados.`,
+        leftSideText: `[b]
+        ¿Tu colchón necesita una limpieza profunda y eliminación de manchas?[/b]
+ Con más de 10 años de experiencia, en limpiarmuebles.pro ofrecemos servicios profesionales de limpieza de colchones a domicilio, especializados en la eliminación de manchas, ácaros, bacterias y alérgenos.
+ Utilizamos productos y equipos de limpieza profesionales para obtener los mejores resultados.`,
         leftSideButtonMarginTop: "60px",
     },
 
