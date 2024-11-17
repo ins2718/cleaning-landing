5efc87979b1896b9d063edd6fcb80b7d494c00f8
Dmitriy <nadim200763@gmail.com>
2024-11-17 12:48:28 +0300

    video

--- src/app/options.ts
+++ src/app/options.ts
@@ -159,6 +159,9 @@ const options = {
         subtitle: "",
         videos: [
             {
+                original: "dy8zLn1uF20",
+                thumbnail: "/images/video-thumbs/sofa-yellow.jpg",
+            },{
                 original: "9X9FFHy6hZg",
                 thumbnail: "/images/video-thumbs/sofa-light.png",
             }, {
@@ -168,8 +171,9 @@ const options = {
                 original: "a2np9IDDTHQ",
                 thumbnail: "/images/video-thumbs/sofa-brown.png",
             },
+
         ],
-        mute: true,
+        mute: false,
     },
 
     faqSection: {
