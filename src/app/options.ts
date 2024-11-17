c18b32d9b951bf774cabbd1df3596bd1089d80d2
Dmitriy <nadim200763@gmail.com>
2024-11-17 23:50:05 +0300

    fix

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,9 +26,9 @@ const options = {
 
     firstSection: {
         title: "Limpieza Profesional de Sofás en Madrid: Consulta Nuestros Precios para Limpieza de Sofás",
-        showTitlePicture: false,
+        showTitlePicture: true
         showLogoTitle: false,
-        showGallery: false,
+        showGallery: true,
         topMargin: "130px",
         bottomMargin: "170px",
         leftSideText: `[b]¿Tus sofás, colchón o alfombra necesitan una limpieza profunda?[/b]
