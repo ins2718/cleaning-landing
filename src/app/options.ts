ab83b3dad50d0c3daf792c9e505c38a4d95bc358
Dmitriy <nadim200763@gmail.com>
2024-11-18 00:14:55 +0300

    change place  ourWorksSection:

--- src/app/options.ts
+++ src/app/options.ts
@@ -116,6 +116,14 @@ const options = {
             root: '/images/our-works/compare/',
             images: [
                 {
+                    path: 'sofa_light/',
+                    before: 'before.jpg',
+                    after: 'after.jpg',
+                }, {
+                    path: 'sofa_orange/',
+                    before: 'before.jpg',
+                    after: 'after.jpg',
+                },{
                     path: 'mattress/',
                     before: 'before.jpg',
                     after: 'after.jpg',
@@ -131,15 +139,7 @@ const options = {
                     path: 'sofa_corner_black/',
                     before: 'before.jpg',
                     after: 'after.jpg',
-                }, {
-                    path: 'sofa_light/',
-                    before: 'before.jpg',
-                    after: 'after.jpg',
-                }, {
-                    path: 'sofa_orange/',
-                    before: 'before.jpg',
-                    after: 'after.jpg',
-                },
+                }, 
             ],
         },
     },
