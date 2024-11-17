d2797772d069a77149acff02959081c35b32a4ed
Dmitriy <nadim200763@gmail.com>
2024-11-17 12:26:12 +0300

    title and gallary add

--- src/app/options.ts
+++ src/app/options.ts
@@ -26,9 +26,9 @@ const options = {
 
     firstSection: {
         title: "Limpieza de Tapicería en Madrid - Consulta Nuestros Precios Atractivos",
-        showTitlePicture: false,
+        showTitlePicture: true,
         showLogoTitle: false,
-        showGallery: false,
+        showGallery: true,
         topMargin: "130px",
         bottomMargin: "170px",
         leftSideText: `[b]¿Tus muebles, colchón o alfombra necesitan una limpieza profunda?[/b]
