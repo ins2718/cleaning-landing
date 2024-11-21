77b140ab43f814b2d530a32845e77e00dd718e10
Dmitriy <nadim200763@gmail.com>
2024-11-21 15:36:10 +0300

    text pic chooseFurnitureSection

--- src/app/options.ts
+++ src/app/options.ts
@@ -30,7 +30,7 @@ const options = {
         showLogoTitle: false,
         showGallery: true,
         topMargin: "130px",
-        bottomMargin: "170px",
+        bottomMargin: "70px",
         leftSideText: `[b]
     ¿Tus sillas necesitan una limpieza profunda?[/b]
  En limpiarmuebles.pro, ofrecemos servicios especializados para limpiar sillas a domicilio con resultados profesionales.
@@ -40,9 +40,9 @@ const options = {
     },
 
     chooseFurnitureSection: {
-        title: "Calcula el precio de la limpieza de tu sofá en minutos",
-        subtitle: "¿Cuál se parece al tuyo?",
-        showImage: true,
+        title: "Calcula el precio de la limpieza de tus sillas en minutos.",
+        subtitle: "¿Cuántas sillas desea limpiar?",
+        showImage: false,
         // @see "../components/main/choose-furnitute-section/right-side.tsx"
     },
 
