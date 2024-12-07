160a7d3ebb13c2cfa88aae52c75583e2e94841d1
Dmitriy <nadim200763@gmail.com>
2024-12-07 21:35:45 +0300

    Google tag

--- src/app/options.ts
+++ src/app/options.ts
@@ -7,7 +7,7 @@ const options = {
 
     facebookPixelId: "347504069641224",
     yandexMetricaIds: [96729923],
-    gtmId: "",
+    gtmId: "GTM-NCFGZ5PX",
 
     phone: "+34603450377",
     email: "nadim200763@gmail.com",
