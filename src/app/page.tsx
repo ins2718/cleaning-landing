4268ffe438e5721b0218338d946e835230b37489
ins <alex.inspektor@gmail.com>
2024-12-07 15:41:15 +0300

    add gtm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,19 @@ import Main from "@/components/main";
 import Overlay from "@/components/overlay";
 import dynamic from "next/dynamic";
 import LocalStorageInitializer from "@/components/local-storage-initializer";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
+import options from "./options";
 const RenderOnViewportEntry = dynamic(() => import("@/components/render-on-viewport-entry"), { ssr: false });
 const Footer = dynamic(() => import("@/components/footer"), { ssr: false });
 
 export default function Home() {
+  useEffect(() => {
+    const gtmId = options.gtmId;
+    if (gtmId) {
+      TagManager.initialize({ gtmId });
+    }
+  }, []);
   return (
     <Provider store={store}>
       <LocalStorageInitializer>
