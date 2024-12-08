17de7cd985867b16ea6a27b39e9b291795d8199f
ins <alex.inspektor@gmail.com>
2024-12-08 19:12:10 +0300

    add lead_Click_class

--- src/components/overlay/order-form-dialog/send-button.tsx
+++ src/components/overlay/order-form-dialog/send-button.tsx
@@ -1,7 +1,7 @@
 import ButtonBasic from "@/components/buttons/button-basic";
 
 function SendButton({ onClick, disabled = false, text = "Ordenar limpieza" }: PropsWithOnClick) {
-    return <ButtonBasic disabled={disabled} type="submit" onClick={onClick} content={text} className="border-[#e23936] bg-[#e23936] hover:shadow-[0_10px_15px_rgba(226,57,54,0.22)]" />;
+    return <ButtonBasic disabled={disabled} type="submit" onClick={onClick} content={text} className="border-[#e23936] bg-[#e23936] hover:shadow-[0_10px_15px_rgba(226,57,54,0.22)] lead_Click_class" />;
 }
 
 export default SendButton;
