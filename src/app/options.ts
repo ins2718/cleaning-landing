5fc6a8bb5767687d9d727b824d3b0195650f3bb4
Dmitriy <nadim200763@gmail.com>
2024-12-13 23:18:01 +0300

    gtmId: "GTM-GDK555RH"

--- src/app/options.ts
+++ src/app/options.ts
@@ -7,7 +7,7 @@ const options = {
 
     facebookPixelId: "347504069641224",
     yandexMetricaIds: [96729923],
-    gtmId: "",
+    gtmId: "GTM-GDK555RH",
 
     phone: "+34603450377",
     email: "nadim200763@gmail.com",
