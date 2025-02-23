77218adfd54f14e8678eecb1a844fa036534ac21
Dmitriy <nadim200763@gmail.com>
2025-02-24 00:15:29 +0400

    fix firstSection

--- src/app/options.ts
+++ src/app/options.ts
@@ -33,7 +33,7 @@ const options = {
     ],
 
     firstSection: {
-        title: "Ремонт в Барселоне, гарантия и сроки по договору, работаем для вас с 2009 года!",
+        title: "Ремонт в Барселоне, гарантия и сроки по договору, работаем с 2009 года!",
         showTitlePicture: true,
         showLogoTitle: false,
         showGallery: true,
